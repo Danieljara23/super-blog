ab16dfab589fc9191694cf6fba4a53839f301ab4
Jara2392 <daniel.jaramillog@udea.edu.co>
2016-11-05 13:01:54 -0500

    New and create actions are working. Bootstrap is interested

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,17 @@
 class PostsController < ApplicationController
+
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = 'The post have been saved'
+      redirect_to post_path(id: @post.id, it_was: 'created')
+    else
+      render :new
+    end
   end
 
   def edit
@@ -8,12 +20,15 @@ class PostsController < ApplicationController
   def index
   end
 
-  def create
-  end
-
   def show
   end
 
   def delete
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title,:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/new'
 
-  get 'posts/edit'
-
-  get 'posts/index'
-
-  get 'posts/create'
-
-  get 'posts/show'
-
-  get 'posts/delete'
+resources :posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
